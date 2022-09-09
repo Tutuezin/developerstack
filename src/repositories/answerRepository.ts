1f19321060b3f75b95b28d89f1002c6cb70c1194
Arthur Alcantara <arthuralcantara.dev@gmail.com>
2022-09-08 21:45:44 -0300

    hmm pipoquinha na manteiga

--- src/repositories/answerRepository.ts
+++ src/repositories/answerRepository.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../config/database";
 import { IAnswerData } from "../types/answerTypes";
 
-export async function createAnswer(answer: any, id: number) {
+export async function createAnswer(answer: IAnswerData, id: number) {
   answer.questionId = id;
 
   await prisma.answers.create({ data: answer });
